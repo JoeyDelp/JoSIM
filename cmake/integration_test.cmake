4d6b8465eaea092cb80c584e7a4e27e9dddd5036
Paul le Roux <pleroux0@outlook.com>
2019-02-13 15:47:49 +0200

    Polish up cmake changes

--- cmake/integration_test.cmake
+++ cmake/integration_test.cmake
@@ -1,11 +1,10 @@
 function(add_integration_test)
-  set(option_args)
-  set(single_args NAME CIR OUT OVERWRITE_ARGS)
-  set(multi_args)
+  set(option_args WRSPICE)
+  set(single_args NAME CIR OUT)
+  set(multi_args OVERWRITE_ARGS)
   cmake_parse_arguments(TEST "${option_args}" "${single_args}" "${multi_args}" ${ARGN})
 
   set(TEST_BUILD_DIR "${CMAKE_BINARY_DIR}/tests/integration/${TEST_NAME}")
-  configure_file("${CMAKE_SOURCE_DIR}/test/${TEST_CIR}" "${TEST_BUILD_DIR}/${TEST_CIR}")
 
   set(JOSIM_COMMAND JoSIM)
 
@@ -13,10 +12,19 @@ function(add_integration_test)
     set(JOSIM_COMMAND ${JOSIM_COMMAND} "-o" "${TEST_OUT}")
   endif()
 
-  set(JOSIM_COMMAND ${JOSIM_COMMAND} "${TEST_CIR}")
+  if(${TEST_WRSPICE})
+    set(JOSIM_COMMAND ${JOSIM_COMMAND} "-c" "1")
+  endif()
+
+  if(DEFINED TEST_CIR)
+    configure_file("${CMAKE_SOURCE_DIR}/test/${TEST_CIR}" "${TEST_BUILD_DIR}/${TEST_CIR}")
+    set(JOSIM_COMMAND ${JOSIM_COMMAND} "${TEST_CIR}")
+  endif()
 
   if(DEFINED TEST_OVERWRITE_ARGS)
-    set(JOSIM_COMMAND ${JOSIM_COMMAND} ${TEST_OVERWRITE_ARGS})
+    set(JOSIM_COMMAND JoSIM ${TEST_OVERWRITE_ARGS})
+  else()
+
   endif()
 
   add_test(
