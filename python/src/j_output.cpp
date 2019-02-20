a8937993d3a0a78a4918a82f9be42eae49846840
Paul le Roux <pleroux0@outlook.com>
2019-02-20 14:20:33 +0200

    Expose traces and timesteps as numpy objects

--- python/src/j_output.cpp
+++ python/src/j_output.cpp
@@ -1,14 +1,35 @@
 #include <pybind11/iostream.h>
+#include <pybind11/numpy.h>
 #include <pybind11/pybind11.h>
+#include <pybind11/stl_bind.h>
 
 #include "j_output.h"
 
 namespace py = pybind11;
 
+PYBIND11_MAKE_OPAQUE(std::vector<Trace>);
+
 namespace pyjosim {
 
 // using namespace JoSim
 void output(py::module &m) {
+  py::class_<Trace>(m, "Trace")
+      .def_readonly("name", &Trace::name)
+      .def("get_data", [](Trace &trace) {
+        std::vector<double> &vec = [&]() -> std::vector<double> & {
+          if (trace.pointer)
+            return *trace.traceData;
+          else
+            return trace.calcData;
+        }();
+
+        double *data = vec.data();
+        size_t size = vec.size();
+        return py::array_t<double>(size, data);
+      });
+
+  py::bind_vector<std::vector<Trace>>(m, "VectorTrace");
+
   py::class_<Output>(m, "Output")
       .def(py::init<>())
       .def("relevant_traces", &Output::relevant_traces)
@@ -20,7 +41,19 @@ void output(py::module &m) {
              py::scoped_ostream_redirect cout;
              py::scoped_estream_redirect cerr;
              output.write_cout(matrix, simulation);
-           });
+           })
+      .def("get_timesteps",
+           [](Output &output) {
+             double *data = output.timesteps->data();
+             size_t size = output.timesteps->size();
+             return py::array_t<double>(size, data);
+           })
+      .def("get_traces", [](Output &output) {
+        // Trace* data = output.traces.data();
+        // size_t size = output.traces.size();
+        // return TraceBuffer{data, size};
+        return output.traces;
+      });
 }
 
 } // namespace pyjosim
