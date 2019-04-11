dc148ea1f1279429a00295c130139a1a71f8cdd5
Paul le Roux <pleroux0@outlook.com>
2019-04-11 16:03:55 +0200

    feat: Suppress output of transient simulation

--- python/src/j_simulation.cpp
+++ python/src/j_simulation.cpp
@@ -6,6 +6,27 @@ namespace py = pybind11;
 
 namespace pyjosim {
 
+class null_buffer : public std::streambuf
+{
+  public:
+  int overflow(int c) { return c; }
+};
+
+class scoped_cout_null {
+  null_buffer null_buffer_;
+  std::streambuf* cache_;
+
+public:
+  scoped_cout_null() {
+    cache_ = std::cout.rdbuf();
+    std::cout.rdbuf(std::addressof(null_buffer_));
+  }
+
+  ~scoped_cout_null() {
+    std::cout.rdbuf(cache_);
+  }
+};
+
 // using namespace JoSim
 void simulation(py::module &m) {
   py::class_<Simulation>(m, "Simulation")
@@ -18,10 +39,12 @@ void simulation(py::module &m) {
            })
       .def("transient_voltage_simulation",
            [](Simulation &simulation, Input &input, Matrix &matrix) {
+             scoped_cout_null cout;
              simulation.transient_voltage_simulation(input, matrix);
            })
       .def("transient_phase_simulation",
            [](Simulation &simulation, Input &input, Matrix &matrix) {
+             scoped_cout_null cout;
              simulation.transient_phase_simulation(input, matrix);
            });
 }
